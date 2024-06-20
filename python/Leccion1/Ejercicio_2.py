e864e45d4b0259190e35f7d9cb155bbfac0776dd
Carina Londero <carilondero@gmail.com>
2024-06-20 17:10:55 -0300

    clase 5 operadores

---
+++ python/Leccion1/Ejercicio_2.py
@@ -0,0 +1,5 @@
+titulo = input("Ingrese el titulo de un libro: ")
+autor = input("Ingrese el nombre del autor: ")
+print("Proporciona el titulo: ", titulo)
+print("Proporciona el autor: ", autor)
+print(titulo, " fue escito por ", autor)
