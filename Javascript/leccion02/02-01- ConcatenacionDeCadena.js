fca4ef9189c5daa4cb4bd760927d77a132c5523b
Carina Londero <carilondero@gmail.com>
2024-06-25 17:30:40 -0300

    Agregado codigo para clase 7. Tag: clase-7-js

--- Javascript/leccion02/02-01- ConcatenacionDeCadena.js
+++ Javascript/leccion02/02-01- ConcatenacionDeCadena.js
@@ -20,3 +20,24 @@ console.log(juntos);
 nombre += apellido;
 console.log(nombre);
 
+//hoy ya no se usa car, se usa let y const
+
+let nombre2 = 'Gabriel';
+console.log(nombre2);
+
+const apellido2 = "Pellicer";
+// apellido2 = 'Perez'; Una constante no puede ser modificada
+console.log(apellido2);
+
+let x, y; // se puede crear varias vbles dentro de una misma linea
+x= 17, y = 21;// se puede asignar varias vbles dentro d una misma linea
+let z = x + y; // se asigna el valor de una operacion
+console.log(z);
+
+let $1num = 31; //no podemos iniciar el nombre d una vbe con un numero 
+// no podemos usar palabras reservadas ej: break
+let rompimiento = 'rompe';
+console.log($1num);
+console.log(rompimiento);
+
+
---
+++ Javascript/leccion03/03-01-OperadoresJavaScript.js
@@ -0,0 +1,23 @@
+//Ejercicio para encontrar nuemros parees
+
+let num = 10;
+
+if (num % 2 == 0){
+    console.log("Es un numero Par");
+}
+else{
+    console.log('Es un numero Impar');
+}
+
+
+
+// Ejercicio es mayor de edad
+let edad = 15;
+let mayor = 18;
+
+if ( edad >= mayor){
+  console.log("Es Mayor de edad");
+}
+else{
+  console.log("Es menor de edad");
+}
