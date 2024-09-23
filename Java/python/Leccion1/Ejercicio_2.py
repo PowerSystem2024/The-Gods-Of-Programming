094b9182192d4c363b82b4699ae20e3ce2721bd7
carina londero <carilondero@gmail.com>
2024-09-23 19:20:30 -0300

    actualizamos carpetas

--- Java/python/Leccion1/Ejercicio_2.py
+++
@@ -1,5 +0,0 @@
-titulo = input("Ingrese el titulo de un libro: ")
-autor = input("Ingrese el nombre del autor: ")
-print("Proporciona el titulo: ", titulo)
-print("Proporciona el autor: ", autor)
-print(titulo, " fue escito por ", autor)
--- Java/python/leccion2/Ejercicio2.py
+++
@@ -1,7 +0,0 @@
-"""
-Ejercicio 2:
-Determinar la solución lógica de la siguiente operación.
-
-((3 + 5 x 8 ) < 3 and ((- 6/3 x 4 ) + 2 < 2)) or ( a > b)
-
-"""
