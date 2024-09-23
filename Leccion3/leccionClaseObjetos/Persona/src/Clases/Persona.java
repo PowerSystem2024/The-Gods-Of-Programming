3f3787db940babda519f26a1f5dfc3526b7f026d
carina londero <carilondero@gmail.com>
2024-09-23 18:54:39 -0300

    Actualizamos Carpeta JAva

---
+++ Leccion3/leccionClaseObjetos/Persona/src/Clases/Persona.java
@@ -0,0 +1,17 @@
+
+package Clases;
+
+/**
+ *
+ * @author gapel
+ */
+public class Persona {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
