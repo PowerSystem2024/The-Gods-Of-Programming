dcb5c8759ef4cf34aa30fe7b87e8ee2add8f02b1
carina londero <carilondero@gmail.com>
2024-11-02 20:27:40 -0300

    Clase9 de java con herencia

---
+++ Java/Leccion6/Herencia/src/domain/Persona.java
@@ -0,0 +1,64 @@
+
+package domain;
+
+
+public class Persona {
+    private String nombre;
+    private char genero;
+    private int edad;
+    private String direccion;
+    
+    //Constructor vacio esto es para crear objetos sin necesidad de inicializar  los atributos de la clase
+    
+    public Persona(){ // constructor
+        
+    }
+     public Persona (String nombre) { //constructor2
+         this.nombre = nombre;
+         
+     }
+
+    public Persona(String nombre, char genero, int edad, String direccion) { //constructor 3
+        this.nombre = nombre;
+        this.genero = genero;
+        this.edad = edad;
+        this.direccion = direccion;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public char getGenero() {
+        return genero;
+    }
+
+    public void setGenero(char genero) {
+        this.genero = genero;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+   
+     
+     
+}
+
