c02cf60fe7256126aba2c42fef087f4e50186f53
Juan Pablo Lopez <juanpi.lz107@gmail.com>
2024-11-10 18:50:02 -0300

    Portfolio Juan Pablo Lopez

---
+++ Portfolio/Portfolio-Juan-Lopez/src/components/MiExperiencia.vue
@@ -0,0 +1,60 @@
+<script setup>
+const grupo = { nombre: "Mi experiencia"};
+const miembros = [
+    {id:1, nombre: ' Primer año de ingenieria en sistemas ', descripcion: 'En ese año comence a conocer el mundo de la programacion y me adentre en el rubro'}, 
+    {id:2, nombre: "Curso tecnico en arreglo de computadoras(sin titulo)", descripcion: "Aprendi lo basico de como elegir bien los componentes de tu pc, tambien como armarla y en caso de fallos en el equipo a arreglarla."}, 
+    {id:3, nombre: "Idiomas", descripcion:"Inglés avanzado (b2)"}]
+    </script>
+
+<template>
+  <div id="app">
+    <div class="container">
+      <h1>{{ grupo.nombre }}</h1>
+      <p>{{ grupo.descripcion }}</p>
+      <ul>
+        <li v-for="miembro in miembros" :key="miembro.id">
+          <strong>{{ miembro.nombre }}</strong>
+          <p>{{ miembro.descripcion }}</p>
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.container {
+    max-width: 800px;
+    margin: 0 auto;
+    padding: 20px;
+    background-color: #555964;
+    border-radius: 10px;
+    box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+    font-family: Arial, sans-serif;
+    color: #ffffff;
+    
+}
+
+h1 {
+    font-size: 28px;
+    margin-bottom: 30px; /* Ajustado para un espaciado más equilibrado */
+}
+
+p {
+    font-size: 16px;
+    line-height: 1.6; /* Ligeramente aumentado para una mejor legibilidad */
+}
+
+ul {
+    list-style: none;
+    padding: 0;
+}
+
+li {
+    margin-bottom: 15px; /* Reducido para un espaciado más compacto */
+}
+
+strong {
+    font-weight: bold;
+    color: #cacee2;
+}
+</style>
---
+++ Portfolio/Portfolio-Juan-Lopez/src/components/icons/NavBar.vue
@@ -0,0 +1,53 @@
+<template>
+    <nav class="navbar">
+            <div class="navbar-menu">
+                <ul>
+<!--Se coloco el operador v-bind al atributo href utilizando su minima expreción el operador : / y se soluciono el enlace a las distintas secciones-->
+                    <a v-for="nav in navegacion" :key="nav.nombre" :href="nav.enlace" class="nav-item" >{{nav.nombre}}</a>  
+                </ul>
+            </div>
+        </nav>
+</template>
+<script setup>
+import { ref } from 'vue';
+const navegacion= ref([
+    {id:1, nombre: "Experiencia", enlace:'#Experiencia'},
+    {id:2, nombre: "Lenguajes", enlace:'#Lenguajes'},
+    {id:3, nombre: "Proyecto", enlace:'#Proyecto'},
+    {id:4, nombre: "Más Sobre Mi", enlace:'#Más'}
+]);
+</script>
+
+<style scoped>
+/* Estilos para la barra de navegación */
+.navbar {
+    background-color: var(--vt-c-indigo); /* Establece el color de fondo usando una variable CSS */
+    color: #ffffffe5; /* Establece el color del texto en blanco */
+    padding: 0.5rem 1rem; /* Añade un padding de 0.5rem arriba y abajo, y 1rem a los lados */
+    display: flex;
+    justify-content: space-between; /* Alinea los elementos al inicio y al final de la navbar */
+    align-items: center; /* Centra verticalmente los elementos dentro de la navbar */
+}
+
+/* Estilos para los elementos de la barra de navegación */
+.navbar-item {
+    color: #fff; /* Establece el color del texto en blanco */
+    text-decoration: none; /* Elimina el subrayado de los enlaces */
+    margin-right: 1rem; /* Añade un margen derecho de 1rem entre los elementos */
+}
+
+/* Estilos para los enlaces */
+a {
+    border: 1px solid; /* Añade un borde sólido de 1px */
+    border-color: hsla(54, 100%, 80%, 0.651); /* Define el color del borde usando hsla */
+    border-radius: 5px; /* Redondea las esquinas del borde */
+    text-decoration: none; /* Elimina el subrayado de los enlaces */
+    transition: 0.4s; /* Añade una transición suave de 0.4s para los cambios de estilo */
+    padding: 5px; /* Añade un padding de 5px alrededor del contenido */
+}
+
+/* Estilos al pasar el mouse sobre los enlaces */
+a:hover {
+    background-color: hsla(54, 93%, 83%, 0.918); /* Cambia el color de fondo al pasar el mouse sobre un enlace */
+}
+</style>
